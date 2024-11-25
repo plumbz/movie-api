fdc1a3fdfbce5ea7770f7eca92ce37f23a7c9c73
plumbz <plumby.eschenbacher@yahoo.com>
2024-11-25 22:26:12 +0100

    added netlilfy to allowed origins

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const express = require('express'),
 bodyParser = require('body-parser'),
   uuid = require('uuid');
 
-let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234'];
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234', 'https://my-testflix.netlify.app'];
 
 
 const { check, validationResult } = require('express-validator');
